5437f44dbb07c065dfc74f200890d63f7d89ca71
Junghyun Colin Kim <colin.kim@groundx.xyz>
2021-01-11 13:58:01 +0900

    Prohibit more than 2 CNs
    It is more complicated to provide account unlock features for 2 or more
    CNs.

--- 1.prepare.sh
+++ 1.prepare.sh
@@ -5,6 +5,11 @@ NUM_CNS=1
 PRIVATE_KEY=492cf9f1e9574e7f9f3e914dcf18117280cf238776ca715da47996ac1b36b98b
 ADDRESS=0x09D790C4717dF9F71382b243f2A0B1DE34a36120
 
+if [ 1 -ne $NUM_CNS ]; then
+  echo "Currently, it only works for 1 CN configuration."
+  exit
+fi
+
 # Download klaytn docker image.
 docker pull klaytn/klaytn:latest
 
@@ -12,7 +17,11 @@ docker pull klaytn/klaytn:latest
 docker run --rm -v ${PWD}:/homi-output klaytn/klaytn homi setup -o /homi-output --docker-image-id klaytn/klaytn:latest --cn-num $NUM_CNS --chainID $CHAIN_ID --network-id $NETWORK_ID docker
 
 # Update private key if necessary
-./update_genesis_account.sh
+if [ ! -z $PRIVATE_KEY ]; then
+  echo "replacing private key."
+	sed -ie "s/\(.*nodekeyhex \)\(.*\)\(\".*\)/\1$PRIVATE_KEY\3/" docker-compose.yml
+	sed -ie "s/\(.*REWARDBASE=\)\(.*\)\(\'.*\)/\1$ADDRESS\3/" docker-compose.yml
+fi
 
 # Add the whale account to the node wallet.
 PK=`grep "nodekeyhex" docker-compose.yml | sed "s/.*nodekeyhex \(.*\)\".*/\1/"`
--- update_genesis_account.sh
+++
@@ -1,5 +0,0 @@
-if [ ! -z $PRIVATE_KEY ]; then
-	sed -ie "s/\(.*nodekeyhex \)\(.*\)\(\".*\)/\1$PRIVATE_KEY\3/" docker-compose.yml
-	sed -ie "s/\(.*REWARDBASE=\)\(.*\)\(\'.*\)/\1$ADDRESS\3/" docker-compose.yml
-fi
-
